4f61aca8864f794ac7323f74a789f722408d0364
Hu Tiger <huxy99@yahoo.com>
2020-07-06 18:22:39 -0500

    delete Tweet API view

--- tweets/urls.py
+++ tweets/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("add", views.add, name="add"),
     path("<int:tweet_id>", views.tweet, name="tweet"),
-    path("add", views.add, name="add")
+    path("<int:tweet_id>/delete", views.delete, name="delete")
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -43,6 +43,19 @@ def add(request):
         return Response(serializer.data, status=201)
     return Response({}, status=400)
 
+@api_view(['DELETE', 'POST'])
+@permission_classes([IsAuthenticated])
+def delete(request, tweet_id):
+    qs = Tweet.objects.filter(id=tweet_id)
+    if not qs.exists():
+        return Response({}, status=404)
+    qs = qs.filter(user=request.user)
+    if not qs.exists():
+        return Response({"message": "You can not delete this tweet."}, status=404)
+    obj = qs.first()
+    obj.delete()
+    return Response({"message": "Tweet romoved."}, status=200)
+
 def index_pure_django(request, *args, **kwargs):
     qs = Tweet.objects.all()
     tweet_list = [x.serialize() for x in qs]
