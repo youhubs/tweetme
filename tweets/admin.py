d2d29d1ea06ce1e607a82e4d299ef34aea1f2279
Hu Tiger <huxy99@yahoo.com>
2020-07-06 18:48:14 -0500

    add a Likes field to Tweet model

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 
-from tweets.models import Tweet
+from tweets.models import Tweet, TweetLike
 
 # Register your models here.
+class TweetLikeAdmin(admin.TabularInline):
+    model = TweetLike
 
 class TweetAdmin(admin.ModelAdmin):
+    inlines = [TweetLikeAdmin]
     list_display = ['__str__', 'content', 'user']
     search_fields = ['content', 'user__username', 'user__email']
     
     class Meta:
         model = Tweet
 
-admin.site.register(Tweet,TweetAdmin)
+admin.site.register(Tweet, TweetAdmin)
--- tweets/models.py
+++ tweets/models.py
@@ -5,10 +5,17 @@ from django.db import models
 User = settings.AUTH_USER_MODEL
 
 # Create your models here.
+class TweetLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey("Tweet", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
 class Tweet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE) # many users can have many tweets
+    likes = models.ManyToManyField(User, related_name="liking_users", blank=True, through=TweetLike)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ["-id"]
