f5eea0806a09c595bfbd70d9489a3025e9707b36
Hu Tiger <huxy99@yahoo.com>
2020-07-06 12:18:31 -0500

    handling ajax request

--- tweets/views.py
+++ tweets/views.py
@@ -40,12 +40,15 @@ def tweet(request, tweet_id, *args, **kwargs):
     return JsonResponse(data, status=status)
 
 def add(request):
+    # print('ajax:', request.is_ajax())
     form = TweetForm(request.POST or None)
     next_url = request.POST.get("next") or None
     if form.is_valid():
         obj = form.save(commit=False)
         # do other form related logic
         obj.save()
+        if request.is_ajax():
+            return JsonResponse({}, status=201) # 201 create items
         if next_url is not None and is_safe_url(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
         form = TweetForm()
