4ce4a49db8ae080bcb83c98bd74cafa16f3da183
Hu Tiger <huxy99@yahoo.com>
2020-07-06 06:47:18 -0500

    Like button rendering

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,4 @@
+import random
 from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
@@ -10,7 +11,7 @@ def home(request, *args, **kwargs):
 
 def index(request, *args, **kwargs):
     qs = Tweet.objects.all()
-    tweet_list = [{"id": x.id, "content": x.content} for x in qs]
+    tweet_list = [{"id": x.id, "content": x.content, "likes": random.randint(0,100) } for x in qs]
     data = {
         "isUser": False,
         "response": tweet_list
