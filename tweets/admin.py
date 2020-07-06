1df5b604f5654c12eee612d74d686fddb2fac743
Hu Tiger <huxy99@yahoo.com>
2020-07-06 14:00:17 -0500

    add User model

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from tweets.models import Tweet
+
 # Register your models here.
+
+class TweetAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Tweet)
--- tweets/models.py
+++ tweets/models.py
@@ -1,8 +1,12 @@
 from random import randint
+from django.conf import settings
 from django.db import models
+
+User = settings.AUTH_USER_MODEL
 
 # Create your models here.
 class Tweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # many users can have many tweets
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
 
